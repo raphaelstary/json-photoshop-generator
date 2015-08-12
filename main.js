708fa91e1ac1dd0330d4767425649cbe73b2405a
raphaelstary <raphael.stary@gmail.com>
2015-08-12 18:35:51 +0200

    export json with just static positions

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-(function () {
+var transformPSDocumentToH5Scenes = require('./lib/transformPSDocumentToH5Scenes');
+var fs = require('fs');
+
+(function (transformPSDocumentToH5Scenes, fs) {
     "use strict";
 
     var PLUGIN_ID = require("./package.json").name;
     var MENU_ID = "json";
     var MENU_LABEL = "export JSON once";
 
-    var _generator = null,
-        _currentDocumentId = null,
-        _config = null;
+    var _generator = null, _currentDocumentId = null, _config = null;
 
     function init(generator, config) {
         _generator = generator;
@@ -15,13 +16,11 @@
 
         console.log("initializing generator highfive.js JSON with config %j", _config);
 
-        _generator.addMenuItem(MENU_ID, MENU_LABEL, true, false).then(
-            function () {
-                console.log("Menu created", MENU_ID);
-            }, function () {
-                console.error("Menu creation failed", MENU_ID);
-            }
-        );
+        _generator.addMenuItem(MENU_ID, MENU_LABEL, true, false).then(function () {
+            console.log("Menu created", MENU_ID);
+        }, function () {
+            console.error("Menu creation failed", MENU_ID);
+        });
 
         _generator.onPhotoshopEvent("generatorMenuChanged", handleGeneratorMenuClicked);
         _generator.onPhotoshopEvent("currentDocumentChanged", handleCurrentDocumentChanged);
@@ -41,23 +40,22 @@
             return;
         }
 
-
+        generateJSON();
     }
 
-    function requestEntireDocument(documentId) {
-        if (!documentId) {
-            console.log("Determining the current document ID");
-        }
+    function generateJSON() {
+        _generator.getDocumentInfo(undefined, {expandSmartObjects: true}).then(function (document) {
+            var h5Document = transformPSDocumentToH5Scenes(document);
+            var fileName = document.file.substring(0, document.file.lastIndexOf('.')) + '.json';
+            fs.writeFile(fileName, JSON.stringify(h5Document), 'utf8', function (err) {
+                if (err) throw err;
+                console.log('success task: write ' + fileName);
+            });
 
-        _generator.getDocumentInfo(documentId, {expandSmartObjects:true}).then(
-            function (document) {
-                console.log("Received complete document:", stringify(document));
-            },
-            function (err) {
-                console.error("Error in getDocumentInfo:", err);
-            }
-        ).done();
+        }, function (err) {
+            console.error("Error in getDocumentInfo:", err);
+        }).done();
     }
 
     exports.init = init;
-})();
+})(transformPSDocumentToH5Scenes, fs);
